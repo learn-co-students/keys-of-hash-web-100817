91956a2486d016a037d05d522062fec6a2731f34
MatteBru <mbruni616@gmail.com>
2017-09-19 13:14:55 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    arr =[]
+    arguments.each do |arg|
+      arr << self.select{|k,v| arg == v}.keys
+    end
+    arr.flatten
   end
 end
