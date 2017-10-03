35ec53c0008ad8f237e2cfc86c6944208e86b142
github username <github email address>
2017-10-03 13:19:29 -0400

    Final Commit

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    return_keys = []
+    self.each do |keys, data|
+      arguments.each do |x|
+        if x == data
+          return_keys << keys
+        end
+      end
+    end
+          
+  return_keys
+    
   end
 end
